4f89e9c94dbdd9d83c10fe162fba0785cdd49af9
aulandsdalen <Ole@Oles-MacBook-Pro.local>
2014-01-19 02:07:58 +0400

    initial commit

---
+++ compile.sh
@@ -0,0 +1,14 @@
+# created on 01/15/2014 by Valera Antonov
+#!/bin/bash
+
+echo "Compiling $1 from $1.pas"
+fpc -Amacho $1.pas
+if [ "$?" -eq "0" ]; then
+	rm $1.o
+	mv $1 executables/$1
+else
+	exit 255 
+fi
+	echo "running executables/$1"
+	executables/$1
+echo "Bye."
